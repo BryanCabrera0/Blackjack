a5dbf1b8abc7341ec753b9636be12397d8100d4f
Bryan Cabrera <133478274+BryanCabrera0@users.noreply.github.com>
2024-10-08 12:06:59 -0400

    Asking user for input

---
+++ blackjack.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string name;
+    cout << "Welcome to Blackjack!\nPlease Enter Your Name: ";
+    cin >> name;
+    
+    
+    char move;
+    cout << "Hello " << name <<"!\nPlease Enter 'H' or 'S' to Begin: ";
+    cin >> move;
+    
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+}
