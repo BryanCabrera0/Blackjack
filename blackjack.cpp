06906004877af4b4a064902b9515a7b801706f13
Bryan Cabrera <133478274+BryanCabrera0@users.noreply.github.com>
2024-10-11 15:54:17 -0400

    Update blackjack.cpp

--- blackjack.cpp
+++ blackjack.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <string>
 
+
 using namespace std;
 
 int main(){
 
 int deck[50];
-char move;
+
 
     string name;
     cout << "Welcome to Blackjack!\nPlease Enter Your Name: ";
     cin >> name;
     
+    name[0]=toupper(name[0]);
+
     
     char begin;
-    cout << "Hello " << name <<"!\nPlease Enter 'B' to Begin: ";
+    cout << "Hello " << name << "!\nPlease Enter 'B' to Begin: ";
     cin >> begin;
     
 
